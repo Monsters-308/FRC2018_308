5d38dbb1de149c2eb94004051b02c60b3d4cd559
MatteoMoceri <matteomoceri@gmail.com>
2018-01-27 11:29:13 -0500

    Basic Arm Code

--- src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
+++ src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
@@ -1,25 +1,29 @@
 
 package org.usfirst.frc308.FRC2018308.subsystems;
 
+import org.usfirst.frc308.FRC2018308.RobotConstants;
+import org.usfirst.frc308.FRC2018308.RobotMap;
 import org.usfirst.frc308.FRC2018308.commands.*;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
-
-
 
 /**
  *
  */
 public class Arm extends Subsystem {
 
+	public static WPI_TalonSRX armMotor = RobotMap.armMotor;
+	
     @Override
     public void initDefaultCommand() {
-
+    	setDefaultCommand(new TeleopArm());
     }
-
+    
     @Override
     public void periodic() {
-       
+       armMotor.set(RobotConstants.joyArm);
     }
 
 }
