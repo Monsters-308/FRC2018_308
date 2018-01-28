ddf1b1d4e7e5ed27e14b1c7fa281bbbb76cb3991
MatteoMoceri <matteomoceri@gmail.com>
2018-01-27 20:18:07 -0500

    Arm Reimplementation

--- src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
+++ src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
@@ -1,54 +1,28 @@
-
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
-package org.usfirst.frc308.FRC2017308.commands;
-=======
 package org.usfirst.frc308.FRC2018308.commands;
+
 import org.usfirst.frc308.FRC2018308.Robot;
+import org.usfirst.frc308.FRC2018308.subsystems.Chassis;
 
->>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
-=======
-package org.usfirst.frc308.FRC2018308.commands;
->>>>>>> parent of ede964c... Revert "Name Refactoring":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
-public class TeleopArm extends Command {
+public class TeleopArm extends Command{
 
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
-    public armControl() {
-=======
-    public TeleopArm() {
->>>>>>> parent of ede964c... Revert "Name Refactoring":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
+	public TeleopArm() {
+		requires(Robot.arm);
+	}
+	
+	
+	protected void initialize() {
+	}
+	
+	protected void execute() {
+		Robot.arm.periodic();
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-=======
-    public TeleopArm() {
-    	requires(Robot.arm);
->>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
-    }
-
-    @Override
-    protected void initialize() {
-    }
-
-    @Override
-    protected void execute() {
-    	Robot.arm.periodic();
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
-
-    @Override
-    protected void end() {
-    }
-
-    @Override
-    protected void interrupted() {
-    }
 }
--- src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
+++ src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
@@ -1,43 +1,25 @@
-
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/subsystems/Arm.java
-<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/subsystems/Arm.java
-package org.usfirst.frc308.FRC2017308.subsystems;
-=======
-package org.usfirst.frc308.FRC2018308.subsystems;
-
-import org.usfirst.frc308.FRC2018308.commands.*;
->>>>>>> parent of ede964c... Revert "Name Refactoring":src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
-
-import edu.wpi.first.wpilibj.command.Subsystem;
-
-=======
 package org.usfirst.frc308.FRC2018308.subsystems;
 
 import org.usfirst.frc308.FRC2018308.RobotConstants;
 import org.usfirst.frc308.FRC2018308.RobotMap;
-import org.usfirst.frc308.FRC2018308.commands.*;
+import org.usfirst.frc308.FRC2018308.commands.TeleopArm;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
->>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
-public class Arm extends Subsystem {
+public class Arm extends Subsystem{
 
-	public static WPI_TalonSRX armMotor = RobotMap.armMotor;
+	public static final WPI_TalonSRX armMotor = RobotMap.armMotor;
 	
-    @Override
-    public void initDefaultCommand() {
-    	setDefaultCommand(new TeleopArm());
-    }
-    
-    @Override
-    public void periodic() {
-       armMotor.set(RobotConstants.joyArm);
-    }
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		setDefaultCommand(new TeleopArm());
+	}
+
+	public void periodic() {
+		armMotor.set(RobotConstants.joyArm);
+	}
 
 }
-
