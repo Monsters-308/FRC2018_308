087e1b8c537a320360ebc3eb79e738132b01c385
MatteoMoceri <matteomoceri@gmail.com>
2018-01-27 19:26:38 -0500

    Revert "Revert "Revert "Revert "Basic Arm Code""""
    This reverts commit fa466f24552c2e80590dc83410889ebfea4f2cb5.

--- src/org/usfirst/frc308/FRC2017308/commands/armControl.java
+++ src/org/usfirst/frc308/FRC2017308/commands/armControl.java
@@ -1,5 +1,11 @@
 
+<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
 package org.usfirst.frc308.FRC2017308.commands;
+=======
+package org.usfirst.frc308.FRC2018308.commands;
+import org.usfirst.frc308.FRC2018308.Robot;
+
+>>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,8 +13,13 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class armControl extends Command {
 
+<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/commands/armControl.java
     public armControl() {
 
+=======
+    public TeleopArm() {
+    	requires(Robot.arm);
+>>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/commands/TeleopArm.java
     }
 
     @Override
@@ -17,6 +28,7 @@ public class armControl extends Command {
 
     @Override
     protected void execute() {
+    	Robot.arm.periodic();
     }
 
     @Override
--- src/org/usfirst/frc308/FRC2017308/subsystems/Arm.java
+++ src/org/usfirst/frc308/FRC2017308/subsystems/Arm.java
@@ -1,24 +1,37 @@
 
+<<<<<<< HEAD:src/org/usfirst/frc308/FRC2017308/subsystems/Arm.java
 package org.usfirst.frc308.FRC2017308.subsystems;
 
 import org.usfirst.frc308.FRC2017308.commands.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+=======
+package org.usfirst.frc308.FRC2018308.subsystems;
 
+import org.usfirst.frc308.FRC2018308.RobotConstants;
+import org.usfirst.frc308.FRC2018308.RobotMap;
+import org.usfirst.frc308.FRC2018308.commands.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+>>>>>>> parent of fa466f2... Revert "Revert "Revert "Basic Arm Code""":src/org/usfirst/frc308/FRC2018308/subsystems/Arm.java
+
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Arm extends Subsystem {
 
+	public static WPI_TalonSRX armMotor = RobotMap.armMotor;
+	
     @Override
     public void initDefaultCommand() {
-
+    	setDefaultCommand(new TeleopArm());
     }
-
+    
     @Override
     public void periodic() {
-       
+       armMotor.set(RobotConstants.joyArm);
     }
 
 }
