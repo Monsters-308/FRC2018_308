6f451e4f847866db95b45e5eb8ded064ae640d77
MatteoMoceri <matteomoceri@gmail.com>
2018-01-31 11:19:37 -0500

    Added Encoders to RobotMap

--- src/org/usfirst/frc308/FRC2018308/RobotConstants.java
+++ src/org/usfirst/frc308/FRC2018308/RobotConstants.java
@@ -1,22 +1,13 @@
 package org.usfirst.frc308.FRC2018308;
 
 public class RobotConstants {
-
-//OI Constants0
-	public static double forward = -Robot.oi.joystick2.getX();
-	public static double strafe = Robot.oi.joystick1.getY();
-	public static double turn = Robot.oi.joystick1.getX();
 	
-	public static double joyArm = Robot.oi.joystick2.getZ();
-
 //Chassis Constants
 	public static final double ChassisKp = 0.3;
 	public static double ChassisKd;
 	public static double ChassisKi;
 	public static double angle;
-	
-	//double speed = encoder.getValue();
-	
+
 //Arm Constants
 	public static double ArmKp;
 	public static double ArmKd;
