274b7283b10c3057f61aea3af6c81722ee499160
MatteoMoceri <matteomoceri@gmail.com>
2018-02-03 14:38:36 -0500

    Encoder Debug and Autonomous

---
+++ src/org/usfirst/frc308/FRC2018308/commands/TeleopPneumatics.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc308.FRC2018308.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TeleopPneumatics extends Command{
+	
+	public TeleopPneumatics(){
+		
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void execute() {
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
